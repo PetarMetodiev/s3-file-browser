52a9006802db59280546767382494b9ef0cc9e74
Petar Metodiev <petar.metodiev@gmail.com>
2023-10-07 18:59:03 +0300

    add sample integration with s3

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,167 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
+import {
+  GetObjectCommand,
+  GetObjectCommandInput,
+  ListObjectsV2Command,
+  ListObjectsV2CommandInput,
+  PutObjectCommand,
+  PutObjectCommandInput,
+  S3Client,
+  _Object,
+} from "@aws-sdk/client-s3";
 
-// When no region or credentials are provided, the SDK will use the
-// region and credentials from the local AWS config.
+const Bucket = localStorage.getItem("bucket") || "";
+
 const client = new S3Client({
-  region: "eu-central-1",
+  region: localStorage.getItem("region") || "",
   credentials: {
-    accessKeyId: "AKIAZ5RCAHL6DB3HWD54",
-    secretAccessKey: "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",
+    accessKeyId: localStorage.getItem("accessKeyId") || "",
+    secretAccessKey: localStorage.getItem("secretAccessKey") || "",
   },
 });
 
-const helloS3 = async () => {
-  const input = {
-    // ListObjectsV2Request
-    Bucket: "interview-task-p-metodiev-a6fea8d1ad38c246", // required
+type ObjectData = {
+  key: string;
+  content: string;
+};
+
+const getAllObjects = () => {
+  const input: ListObjectsV2CommandInput = {
+    Bucket,
   };
   const command = new ListObjectsV2Command(input);
-  client.send(command).then((result) => console.log(result.Contents));
+  return client.send(command);
+};
+
+const uploadObject = ({ content, key }: ObjectData) => {
+  // for file upload
+  // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/Package/-aws-sdk-lib-storage/
+  const input: PutObjectCommandInput = {
+    Bucket,
+    Key: key,
+    Body: content,
+  };
+
+  const command = new PutObjectCommand(input);
+  return client.send(command);
+};
+
+const getObject = (key: string) => {
+  const input: GetObjectCommandInput = {
+    Bucket,
+    Key: key,
+  };
+
+  const command = new GetObjectCommand(input);
+  return client.send(command).then((r) => r.Body.transformToString());
 };
 
 function App() {
-  const [count, setCount] = useState(0);
-  helloS3();
+  const [s3data, setS3Data] = useState<_Object[]>();
+  const [objData, setObjData] = useState();
+  const [objKey, setObjKey] = useState("");
+  const [objContent, setObjContent] = useState("");
+  const [region, setRegion] = useState("");
+  const [accessKeyId, setAccessKeyId] = useState("");
+  const [secretAccessKey, setSecretAccessKey] = useState("");
+  const [bucket, setBucket] = useState("");
+
+  const handleUpload = () => {
+    return uploadObject({ content: objContent, key: objKey })
+      .then((res) => console.log("success:", res))
+      .catch((e) => console.log(e));
+  };
+
+  const handleGetObject = () => {
+    getObject(objKey)
+      .then((r) => {
+        setObjData(r);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  const handleGetAllObjects = () => {
+    getAllObjects()
+      .then((r) => r.Contents || [])
+      .then((r) =>
+        r.sort((obj1, obj2) => {
+          return obj2.LastModified?.getTime() - obj1.LastModified?.getTime();
+        })
+      )
+      .then((r) => {
+        setS3Data(r);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  const submitCredentials = () => {
+    console.log("bucket", bucket);
+    console.log("region", region);
+    console.log("accessKeyId", accessKeyId);
+    console.log("secretAccessKey", secretAccessKey);
+    localStorage.setItem("bucket", bucket);
+    localStorage.setItem("region", region);
+    localStorage.setItem("accessKeyId", accessKeyId);
+    localStorage.setItem("secretAccessKey", secretAccessKey);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Enter credentials:</h1>
+        <label>
+          Bucket:
+          <input type="text" onChange={(e) => setBucket(e.target.value)} />
+        </label>
+        <label>
+          Region:
+          <input type="text" onChange={(e) => setRegion(e.target.value)} />
+        </label>
+        <label>
+          Access Key Id:
+          <input type="text" onChange={(e) => setAccessKeyId(e.target.value)} />
+        </label>
+        <label>
+          Secret Access Key:
+          <input
+            type="text"
+            onChange={(e) => setSecretAccessKey(e.target.value)}
+          />
+        </label>
+        <button onClick={submitCredentials}>Submit</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div>
+        <h2>Upload object</h2>
+        <label>
+          File name:
+          <input type="text" onChange={(e) => setObjKey(e.target.value)} />
+        </label>
+        <label>
+          File contents name:
+          <textarea onChange={(e) => setObjContent(e.target.value)} />
+        </label>
+        <hr />
+        {objKey} / {objContent}
+        <button
+          onClick={() => {
+            handleUpload().then(handleGetAllObjects);
+          }}
+        >
+          Upload
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <h2>Get Single object</h2>
+        <button onClick={handleGetObject}>Get single object data</button>
+        <pre>{objData}</pre>
+      </div>
+      <div>
+        <h2>Get all objects</h2>
+        <button onClick={handleGetAllObjects}>Get all objects data</button>
+        <pre>{JSON.stringify(s3data, null, 2)}</pre>
+      </div>
     </>
   );
 }
