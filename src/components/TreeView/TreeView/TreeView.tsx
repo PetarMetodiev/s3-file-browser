6cde7046352e2c6537d57f2a8c3f32687de339e9
Petar Metodiev <petar.metodiev@gmail.com>
2023-10-12 18:39:16 +0300

    add root actions to the tree view

--- src/components/TreeView/TreeView/TreeView.tsx
+++ src/components/TreeView/TreeView/TreeView.tsx
@@ -1,30 +1,20 @@
 import { TreeNode } from "../TreeNode/TreeNode";
-import { useContext, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 
 import "./TreeView.css";
 import { FileContentsContext } from "@src/contexts/FileContentsContextProvider";
 import { RawObj } from "@src/utils/convertToTreeStructure";
 import { Button } from "@src/components/form-controls/Button/Button";
 
-export const TreeView = () => {
-  const {
-    isLoading,
-    fetchDirectoryContents,
-    showNewFileInput,
-    showNewDirectoryInput,
-  } = useContext(FileContentsContext);
-  const [paths, setPaths] = useState<RawObj[] | undefined>();
-  // const paths: RawObj[] | undefined = objectKeys
-  //   ?.map((o) => ({
-  //     key: o.Key,
-  //     isDir: o.Size === 0,
-  //   }))
-  //   .sort((a, b) => Number(b.isDir) - Number(a.isDir));
-  //
-  // console.log({ paths });
-  // const paths = objectKeys?.map((o) => o.Key) || ["asd", "ddd", "ddd/fff"];
+import "css.gg/icons/css/file-add.css";
+import "css.gg/icons/css/folder-add.css";
 
-  useEffect(() => {
+export const TreeView = () => {
+  const { fetchDirectoryContents, showNewFileInput, showNewDirectoryInput } =
+    useContext(FileContentsContext);
+  const [paths, setPaths] = useState<RawObj[] | undefined>();
+
+  const refreshDirectoryContents = useCallback(() => {
     fetchDirectoryContents({ path: "0#/" }).then((r) => {
       const ps: RawObj[] | undefined = r
         .map((o) => ({
@@ -36,39 +26,50 @@ export const TreeView = () => {
     });
   }, [fetchDirectoryContents]);
 
+  useEffect(() => {
+    refreshDirectoryContents();
+  }, [refreshDirectoryContents]);
+
   return (
     <div className="tree-view">
-      {paths ? (
-        <ul data-tree-container>
-          {paths.map((p) => {
-            const nodeName = p.key?.split("/").at(-1);
-            return (
-              <TreeNode
-                key={p.key!}
-                nodeName={nodeName!}
-                isDirectory={p.isDir}
-                path={p.key!}
-              />
-            );
-          })}
-          {/* {toTree(paths as string[]).map((n: TreeNodeType) => { */}
-          {/*   return ( */}
-          {/*     <TreeNode */}
-          {/*       path={n.nodeKey} */}
-          {/*       key={`${n.id}-${n.nodeKey}`} */}
-          {/*       nodeKey={n.nodeKey} */}
-          {/*       childNodes={n.childNodes} */}
-          {/*     /> */}
-          {/*   ); */}
-          {/* })} */}
-        </ul>
+      {paths && paths?.length > 0 ? (
+        <div data-tree-container>
+          <ul data-nodes-container>
+            {paths.map((p) => {
+              const nodeName = p.key?.split("/").at(-1);
+              return (
+                <TreeNode
+                  key={p.key!}
+                  nodeName={nodeName!}
+                  isDirectory={p.isDir}
+                  path={p.key!}
+                  onDelete={refreshDirectoryContents}
+                />
+              );
+            })}
+          </ul>
+          <div data-root-actions>
+            <button onClick={() => showNewFileInput({ path: "0#" })}>
+              <i className="gg-file-add"></i>
+            </button>
+            <button onClick={() => showNewDirectoryInput({ path: "0#" })}>
+              <i className="gg-folder-add"></i>
+            </button>
+          </div>
+        </div>
       ) : (
         <div data-no-files>
           Nothing found in the bucket.
-          <Button onClick={() => showNewDirectoryInput()} data-add-files>
+          <Button
+            onClick={() => showNewDirectoryInput({ path: "0#" })}
+            data-add-files
+          >
             Add a dir?
           </Button>
-          <Button onClick={() => showNewFileInput()} data-add-files>
+          <Button
+            onClick={() => showNewFileInput({ path: "0#" })}
+            data-add-files
+          >
             Or a file?
           </Button>
         </div>
