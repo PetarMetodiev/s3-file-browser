cd26f7f6e5aba97bd50ddf69e6c91e383e3dd8fd
Petar Metodiev <petar.metodiev@gmail.com>
2023-10-13 18:02:11 +0300

    fix file paths for form controls

---
+++ src/components/form-controls/Button/Button.tsx
@@ -0,0 +1,19 @@
+import { MouseEventHandler, PropsWithChildren } from "react";
+import "./Button.css";
+
+type ButtonProps = {
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  className?: string;
+};
+
+export const Button = ({
+  onClick,
+  className,
+  children,
+}: PropsWithChildren<ButtonProps>) => {
+  return (
+    <button className={`btn ${className}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
