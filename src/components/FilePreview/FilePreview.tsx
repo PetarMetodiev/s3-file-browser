c02128fe3dd4cc0f03a2065e3e3484d80f98def6
Petar Metodiev <petar.metodiev@gmail.com>
2023-10-14 19:24:42 +0300

    extract content upload forms to separate component

--- src/components/FilePreview/FilePreview.tsx
+++ src/components/FilePreview/FilePreview.tsx
@@ -1,115 +1,40 @@
 import { FileContentsContext } from "@src/contexts/FileContentsContextProvider";
 import "./FilePreview.css";
 import { useContext, useEffect, useState } from "react";
-import { Input } from "../Input/Input";
-import { Button } from "../Button/Button";
+import { UploadContent } from "../UploadContent/UploadContent";
 
 export const FilePreview = () => {
   const {
     fileContents,
     isLoading,
+    currentDirectory,
+    currentFile,
     isNewFileInputVisible,
     isNewDirectoryInputVisible,
-    createDirectory,
-    uploadFile,
-    selectedPath,
-    displayPath,
   } = useContext(FileContentsContext);
 
-  const [newDirectory, setNewDirectory] = useState("");
-  const [directoryNameError, setDirectoryNameError] = useState(false);
-  const [newFileName, setNewFileName] = useState("");
-  const [fileNameError, setFileNameError] = useState(false);
-  const [newFileContent, setNewFileContent] = useState("");
-
   const [displayMessage, setDisplayMessage] = useState("");
+  const showUploadContent = isNewFileInputVisible || isNewDirectoryInputVisible;
 
   useEffect(() => {
-    if (!isNewFileInputVisible && !isNewDirectoryInputVisible) {
-      if (isLoading) {
-        setDisplayMessage("Loading...");
-      } else if (fileContents) {
-        setDisplayMessage(fileContents);
-      } else {
-        setDisplayMessage("Click on a file to see its content");
-      }
+    if (isLoading) {
+      setDisplayMessage("Loading...");
+    } else if (fileContents) {
+      setDisplayMessage(fileContents);
+    } else {
+      setDisplayMessage("Click on a file to see its content");
     }
-  }, [
-    isNewFileInputVisible,
-    isNewDirectoryInputVisible,
-    isLoading,
-    fileContents,
-  ]);
+  }, [isLoading, fileContents]);
 
   return (
     <div className="file-preview">
-      <div data-titlebar>{displayPath && `File: ${displayPath}`}</div>
+      <div data-titlebar>{currentFile && `currentFile: ${currentFile}`}</div>
+      <div data-titlebar>
+        {currentDirectory && `selectedPath: ${currentDirectory}`}
+      </div>
       <div data-main-content>
-        {!isLoading && isNewDirectoryInputVisible && (
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              if (newDirectory.includes("/")) {
-                setDirectoryNameError(true);
-              } else {
-                setDirectoryNameError(false);
-                createDirectory({
-                  directoryName: newDirectory,
-                  path: selectedPath,
-                }).then(() => setNewDirectory(""));
-              }
-            }}
-          >
-            <Input
-              label="Enter new directory name(min 3 chars, no / allowed)"
-              onChange={setNewDirectory}
-              value={newDirectory}
-              required
-              minlength={3}
-            />
-            <Button>Create</Button>
-            {directoryNameError && <span>No /(slash) symbols allowed.</span>}
-          </form>
-        )}
         {/* TODO: Fix loading indicator*/}
-        {!isLoading && isNewFileInputVisible && (
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-
-              if (newFileName.includes("/")) {
-                setFileNameError(true);
-              } else {
-                setFileNameError(false);
-                uploadFile({
-                  content: newFileContent,
-                  fileName: newFileName,
-                  path: selectedPath,
-                }).then(() => {
-                  setNewFileName("");
-                  setNewFileContent("");
-                });
-              }
-            }}
-          >
-            <Input
-              label="Enter new file name(min 3 chars)"
-              onChange={setNewFileName}
-              value={newFileName}
-              required
-              minlength={3}
-            />
-            <Input
-              label="Enter contents(min 3 chars)"
-              onChange={setNewFileContent}
-              value={newFileContent}
-              required
-              minlength={3}
-            />
-            <Button>Upload</Button>
-            {fileNameError && <span>No /(slash) symbols allowed.</span>}
-          </form>
-        )}
+        {!isLoading && showUploadContent && <UploadContent />}
         {displayMessage}
       </div>
     </div>
---
+++ src/components/UploadContent/UploadContent.tsx
@@ -0,0 +1,90 @@
+import { FileContentsContext } from "@src/contexts/FileContentsContextProvider";
+import { useContext, useState } from "react";
+import { Input } from "../Input/Input";
+import { Button } from "../Button/Button";
+
+export const UploadContent = () => {
+  const [newDirectory, setNewDirectory] = useState("");
+  const [directoryNameError, setDirectoryNameError] = useState(false);
+  const [newFileName, setNewFileName] = useState("");
+  const [fileNameError, setFileNameError] = useState(false);
+  const [newFileContent, setNewFileContent] = useState("");
+
+  const {
+    isNewFileInputVisible,
+    isNewDirectoryInputVisible,
+    createDirectory,
+    uploadFile,
+    currentDirectory,
+  } = useContext(FileContentsContext);
+
+  return (
+    <div className="upload-content">
+      {isNewDirectoryInputVisible && (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (newDirectory.includes("/")) {
+              setDirectoryNameError(true);
+            } else {
+              setDirectoryNameError(false);
+              createDirectory({
+                directoryName: newDirectory,
+                path: currentDirectory,
+              }).then(() => setNewDirectory(""));
+            }
+          }}
+        >
+          <Input
+            label="Enter new directory name(min 3 chars, no / allowed)"
+            onChange={setNewDirectory}
+            value={newDirectory}
+            required
+            minlength={3}
+          />
+          <Button>Create</Button>
+          {directoryNameError && <span>No /(slash) symbols allowed.</span>}
+        </form>
+      )}
+      {isNewFileInputVisible && (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+
+            if (newFileName.includes("/")) {
+              setFileNameError(true);
+            } else {
+              setFileNameError(false);
+              console.log({ selectedPath: currentDirectory });
+              uploadFile({
+                content: newFileContent,
+                fileName: newFileName,
+                path: currentDirectory,
+              }).then(() => {
+                setNewFileName("");
+                setNewFileContent("");
+              });
+            }
+          }}
+        >
+          <Input
+            label="Enter new file name(min 3 chars)"
+            onChange={setNewFileName}
+            value={newFileName}
+            required
+            minlength={3}
+          />
+          <Input
+            label="Enter contents(min 3 chars)"
+            onChange={setNewFileContent}
+            value={newFileContent}
+            required
+            minlength={3}
+          />
+          <Button>Upload</Button>
+          {fileNameError && <span>No /(slash) symbols allowed.</span>}
+        </form>
+      )}
+    </div>
+  );
+};
