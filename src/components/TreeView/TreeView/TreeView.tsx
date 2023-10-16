ca49043738f0fbf075e6e1231111d35397bd9835
Petar Metodiev <petar.metodiev@gmail.com>
2023-10-16 13:14:30 +0300

    use tree node as the root node
    On initial load it is not being marked as selected.

--- src/components/TreeView/TreeView/TreeView.tsx
+++ src/components/TreeView/TreeView/TreeView.tsx
@@ -20,90 +20,93 @@ type TreeViewProps = {
 
 export const TreeView = ({ className }: TreeViewProps) => {
   const {
-    fetchDirectoryContents,
+    // fetchDirectoryContents,
     showNewFileInput,
     showNewDirectoryInput,
     isLoading,
-    isNewDirectoryInputVisible,
+    // isNewDirectoryInputVisible,
   } = useContext(FileContentsContext);
   const { logout } = useContext(S3CredentialsContext);
-  const [paths, setPaths] = useState<RawObj[] | undefined>();
-  const [emptyBucket, setEmptyBucket] = useState(false);
+  // const [paths, setPaths] = useState<RawObj[] | undefined>();
+  // const [emptyBucket, setEmptyBucket] = useState(false);
 
-  const refreshDirectoryContents = useCallback(
-    ({ setAsCurrent }: { setAsCurrent: boolean } = { setAsCurrent: false }) => {
-      fetchDirectoryContents({ path: rootPath, setAsCurrent })
-        .then((r) => {
-          setPaths(r);
-          setEmptyBucket(!r || r.length === 0);
-        })
-        .catch();
-    },
-    [fetchDirectoryContents]
-  );
+  // const refreshDirectoryContents = useCallback(
+  //   ({ setAsCurrent }: { setAsCurrent: boolean } = { setAsCurrent: false }) => {
+  //     fetchDirectoryContents({ path: rootPath, setAsCurrent })
+  //       .then((r) => {
+  //         setPaths(r);
+  //         setEmptyBucket(!r || r.length === 0);
+  //       })
+  //       .catch();
+  //   },
+  //   [fetchDirectoryContents]
+  // );
 
-  useEffect(() => {
-    refreshDirectoryContents({ setAsCurrent: true });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // useEffect(() => {
+  //   refreshDirectoryContents({ setAsCurrent: true });
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
   // adding new dirs to the root should refresh the tree
-  useEffect(() => {
-    if (!isNewDirectoryInputVisible) {
-      refreshDirectoryContents();
-    }
-  }, [isNewDirectoryInputVisible, refreshDirectoryContents]);
+  // useEffect(() => {
+  //   if (!isNewDirectoryInputVisible) {
+  //     refreshDirectoryContents();
+  //   }
+  // }, [isNewDirectoryInputVisible, refreshDirectoryContents]);
 
   return (
     <div className={`tree-view-inner ${className || ""}`}>
-      <div data-tree-container>
-        {paths && paths.length > 0 && (
-          <>
-            <ul data-nodes-container>
-              {paths
-                .filter((p) => p.isDir)
-                .map((p) => {
-                  const nodeName = p.key?.split("/").at(-1);
-                  return (
-                    <TreeNode
-                      key={p.key!}
-                      nodeName={nodeName!}
-                      isDirectory={p.isDir}
-                      path={p.key!}
-                    />
-                  );
-                })}
-            </ul>
-          </>
-        )}
-      </div>
-      {!isLoading && emptyBucket && (
-        <div data-no-files>
-          Nothing found in the bucket.
-          <Button
-            onClick={() =>
-              showNewDirectoryInput({
-                path: rootPath,
-                onClose: refreshDirectoryContents,
-              })
-            }
-            data-add-files
-          >
-            Add a dir?
-          </Button>
-          <Button
-            onClick={() =>
-              showNewFileInput({
-                path: rootPath,
-                onClose: refreshDirectoryContents,
-              })
-            }
-            data-add-files
-          >
-            Or a file?
-          </Button>
-        </div>
-      )}
+      {/* <div data-tree-container> */}
+      {/*   {paths && paths.length > 0 && ( */}
+      {/*     <> */}
+      {/*       <ul data-nodes-container> */}
+      {/*         {paths */}
+      {/*           .filter((p) => p.isDir) */}
+      {/*           .map((p) => { */}
+      {/*             const nodeName = p.key?.split("/").at(-1); */}
+      {/*             return ( */}
+      {/*               <TreeNode */}
+      {/*                 key={p.key!} */}
+      {/*                 nodeName={nodeName!} */}
+      {/*                 isDirectory={p.isDir} */}
+      {/*                 path={p.key!} */}
+      {/*               /> */}
+      {/*             ); */}
+      {/*           })} */}
+      {/*       </ul> */}
+      {/*     </> */}
+      {/*   )} */}
+      {/* </div> */}
+      <ul data-tree-container>
+        <TreeNode nodeName="/" isDirectory={true} path={"-1#/"} />
+      </ul>
+      {/* {!isLoading && emptyBucket && ( */}
+      {/*   <div data-no-files> */}
+      {/*     Nothing found in the bucket. */}
+      {/*     <Button */}
+      {/*       onClick={() => */}
+      {/*         showNewDirectoryInput({ */}
+      {/*           path: rootPath, */}
+      {/*           onClose: refreshDirectoryContents, */}
+      {/*         }) */}
+      {/*       } */}
+      {/*       data-add-files */}
+      {/*     > */}
+      {/*       Add a dir? */}
+      {/*     </Button> */}
+      {/*     <Button */}
+      {/*       onClick={() => */}
+      {/*         showNewFileInput({ */}
+      {/*           path: rootPath, */}
+      {/*           onClose: refreshDirectoryContents, */}
+      {/*         }) */}
+      {/*       } */}
+      {/*       data-add-files */}
+      {/*     > */}
+      {/*       Or a file? */}
+      {/*     </Button> */}
+      {/*   </div> */}
+      {/* )} */}
       <Button onClick={logout}>Logout</Button>
     </div>
   );
